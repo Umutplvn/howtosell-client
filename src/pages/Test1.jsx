bf84ddcf1a3bc19b7d314b0138fd2bb82a9f3267
umutplvn <umutpehlivan2078@gmail.com>
2024-10-03 04:07:06 +0100

    up

--- src/pages/Test1.jsx
+++ src/pages/Test1.jsx
@@ -7,12 +7,14 @@ import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "../style/test1.css"; // CSS dosyanızı ekleyin
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
 
 const Test1 = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedValue, setSelectedValue] = useState("");
   const [answers, setAnswers] = useState({});
   const [required, setRequired] = useState(false);
+  
 
   const handleRadioChange = (event) => {
     setSelectedValue(event.target.value);
@@ -28,30 +30,32 @@ const Test1 = () => {
       ...prev,
       [currentQuestion]: event.target.value,
     }));
+    setRequired(false)
   };
 
   const handleNext = () => {
-    if (answers[currentQuestion] === undefined || answers[currentQuestion] === "") {
+    if (
+      answers[currentQuestion] === undefined ||
+      answers[currentQuestion] === ""
+    ) {
       setRequired(true);
       return;
     }
     setRequired(false);
-    setCurrentQuestion((prev) => Math.min(prev + 1, 2)); //! Kac soru oldugunu guncelle
+    setCurrentQuestion((prev) => Math.min(prev + 1, 5)); //! Kac soru oldugunu guncelle
   };
 
   const handlePrev = () => {
     setCurrentQuestion((prev) => Math.max(prev - 1, 0));
+    setRequired(false)
+
   };
 
   console.log(answers);
   return (
     <Box sx={{ height: "100vh", overflow: "hidden", position: "relative" }}>
       <TransitionGroup>
-        <CSSTransition
-          key={currentQuestion}
-          timeout={300}
-          classNames="fade" // CSS sınıfı
-        >
+        <CSSTransition key={currentQuestion} timeout={300} classNames="fade">
           <Box
             sx={{
               width: "100%",
@@ -63,8 +67,8 @@ const Test1 = () => {
               transition: "opacity 0.3s ease-in-out",
             }}
           >
-{/* Question 0 */}
-{currentQuestion === 0 && (
+            {/* Question 0 */}
+            {currentQuestion === 0 && (
               <>
                 <Typography
                   sx={{
@@ -86,21 +90,56 @@ const Test1 = () => {
                 >
                   {["Under 18", "18-24", "25-34", "35-44", "45-54", "55+"].map(
                     (value) => (
-                      <Sheet key={value} sx={{ p: 2, borderRadius: "md", boxShadow: "sm" }}>
-                        <Radio label={value} overlay disableIcon value={value} />
+                      <Sheet
+                        key={value}
+                        sx={{ p: 2, borderRadius: "md", boxShadow: "sm" }}
+                      >
+                        <Radio
+                          label={value}
+                          overlay
+                          disableIcon
+                          value={value}
+                        />
                       </Sheet>
                     )
                   )}
                 </RadioGroup>
-                {required && (
-                  <Typography sx={{ color: "red", mt: 2 }}>
-                    Please answer this question.
-                  </Typography>
-                )}
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "300px",
+                  }}
+                >
+                  {required ? (
+                    <Box
+                      sx={{
+                        backgroundColor: "#F7E6E5",
+                        color: "#bc1616",
+                        p: "0.5rem",
+                        mt: "0.5rem",
+                        mb: "-1rem",
+                        borderRadius: "0.2rem",
+                        width: "9rem",
+                        textAlign: "center",
+                        display: "flex",
+                        gap: "0.2rem",
+                      }}
+                    >
+                      <WarningRoundedIcon style={{ fontSize: "0.98rem" }} />
+                      <Typography sx={{ fontSize: "0.8rem" }}>
+                        Please fill this in
+                      </Typography>
+                    </Box>
+                  ) : (
+                    <Box sx={{ height: "2rem" }}></Box>
+                  )}
+                </Box>
               </>
             )}
-{/* Question 1 */}
-{currentQuestion === 1 && (
+            {/* Question 1 */}
+            {currentQuestion === 1 && (
               <>
                 <Typography
                   sx={{
@@ -120,16 +159,43 @@ const Test1 = () => {
                   sx={{ width: "300px", mt: 2 }}
                   value={answers[currentQuestion] || ""}
                   onChange={handleInputChange}
+                  
                 />
-                {required && (
-                  <Typography sx={{ color: "red", mt: 2 }}>
-                    Please answer this question.
-                  </Typography>
-                )}
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "300px",
+                  }}
+                >
+                  {required ? (
+                    <Box
+                      sx={{
+                        backgroundColor: "#F7E6E5",
+                        color: "#bc1616",
+                        p: "0.5rem",
+                        mt: "0.5rem",
+                        mb: "-1rem",
+                        borderRadius: "0.2rem",
+                        width: "9rem",
+                        textAlign: "center",
+                        display: "flex",
+                        gap: "0.2rem",
+                      }}
+                    >
+                      <WarningRoundedIcon style={{ fontSize: "0.98rem" }} />
+                      <Typography sx={{ fontSize: "0.8rem" }}>
+                        Please fill this in
+                      </Typography>
+                    </Box>
+                  ) : (
+                    <Box sx={{ height: "2rem" }}></Box>
+                  )}
+                </Box>
               </>
             )}
 
-{/* Question 2 */}
+            {/* Question 2 */}
             {currentQuestion === 2 && (
               <>
                 <Typography
@@ -151,21 +217,39 @@ const Test1 = () => {
                   value={answers[currentQuestion] || ""}
                   onChange={handleInputChange}
                 />
-                {required && (
-                  <Typography sx={{ color: "red", mt: 2 }}>
-                    Please answer this question.
-                  </Typography>
+
+                {required ? (
+                  <Box
+                    sx={{
+                      backgroundColor: "#F7E6E5",
+                      color: "#bc1616",
+                      p: "0.5rem",
+                      mt: "0.5rem",
+                      mb: "-1rem",
+                      borderRadius: "0.2rem",
+                      width: "19rem",
+                      textAlign: "center",
+                      display: "flex",
+                      gap: "0.2rem",
+                    }}
+                  >
+                    <WarningRoundedIcon style={{ fontSize: "0.98rem" }} />
+                    <Typography sx={{ fontSize: "0.8rem" }}>
+                      Please fill this in
+                    </Typography>
+                  </Box>
+                ) : (
+                  <Box sx={{ height: "2rem" }}></Box>
                 )}
               </>
             )}
 
+            {/* Question 3 */}
 
           </Box>
         </CSSTransition>
       </TransitionGroup>
-
-
-//! Arrow Icons
+      //! Arrow Icons
       <Box
         sx={{
           position: "fixed",
@@ -186,7 +270,7 @@ const Test1 = () => {
             fontSize: "2rem",
             p: "0.2rem",
             cursor: "pointer",
-            visibility: currentQuestion === 0 ? "hidden" : "visible", // İlk soruda yukarı oku gizle
+            visibility: currentQuestion === 0 ? "hidden" : "visible",
           }}
         />
         <ArrowDownwardIcon
