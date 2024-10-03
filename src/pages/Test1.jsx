cd525b9b115751cb30d702b9e2042c163c6f4ed6
umutplvn <umutpehlivan2078@gmail.com>
2024-10-03 03:18:28 +0100

    up

--- src/pages/Test1.jsx
+++ src/pages/Test1.jsx
@@ -1,49 +1,12 @@
 import React, { useState } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { Box, Typography, Button, TextField } from "@mui/material";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Box, Typography, TextField } from "@mui/material";
 import RadioGroup from "@mui/joy/RadioGroup";
 import Radio from "@mui/joy/Radio";
 import Sheet from "@mui/joy/Sheet";
-import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
-
-const questions = [
-  {
-    id: 1,
-    type: "multiple-choice",
-    question: "How old are you?",
-    options: ["Under 18", "18-24", "24-34", "35-44", "45-54", "55+"],
-    validate: (value) => !!value,
-  },
-  {
-    id: 2,
-    type: "fill-in-the-blank",
-    question: "What is your First Name?",
-    placeholder: "Jane",
-    validate: (value) => !!value,
-  },
-  {
-    id: 3,
-    type: "fill-in-the-blank",
-    question: "What is your Last Name?",
-    placeholder: "Smith",
-    validate: (value) => !!value,
-  },
-  {
-    id: 4,
-    type: "fill-in-the-blank",
-    question: "What's your email address?",
-    placeholder: "name@example.com",
-    validate: (value) => /^\S+@\S+\.\S+$/.test(value), // Email doğrulama
-  },
-  {
-    id: 5,
-    type: "fill-in-the-blank",
-    question: "What's your WhatsApp Number?",
-    placeholder: "+353 (_ _)",
-    validate: (value) => /^\+\d{1,3}\s?\(?\d{1,4}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(value), // Telefon numarası doğrulama
-  },
-];
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "../style/test1.css"; // CSS dosyanızı ekleyin
 
 const Test1 = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -53,156 +16,191 @@ const Test1 = () => {
 
   const handleRadioChange = (event) => {
     setSelectedValue(event.target.value);
+    setAnswers((prev) => ({
+      ...prev,
+      [currentQuestion]: event.target.value,
+    }));
     setRequired(false);
   };
 
   const handleInputChange = (event) => {
     setAnswers((prev) => ({
       ...prev,
-      [questions[currentQuestion].id]: event.target.value,
+      [currentQuestion]: event.target.value,
     }));
-    setRequired(false);
   };
 
   const handleNext = () => {
-    const currentQuestionData = questions[currentQuestion];
-
-    if (currentQuestion < questions.length - 1) {
-      if (!currentQuestionData.validate(selectedValue || answers[currentQuestionData.id])) {
-        setRequired(true);
-        return;
-      }
-
-      setAnswers((prev) => ({
-        ...prev,
-        [currentQuestionData.id]: selectedValue || answers[currentQuestionData.id],
-      }));
-      setSelectedValue("");
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      console.log("Answers:", answers);
+    if (answers[currentQuestion] === undefined || answers[currentQuestion] === "") {
+      setRequired(true);
+      return;
     }
+    setRequired(false);
+    setCurrentQuestion((prev) => Math.min(prev + 1, 2)); //! Kac soru oldugunu guncelle
   };
 
   const handlePrev = () => {
-    if (currentQuestion > 0) {
-      setCurrentQuestion(currentQuestion - 1);
-      setSelectedValue(answers[questions[currentQuestion - 1].id] || "");
-    }
+    setCurrentQuestion((prev) => Math.max(prev - 1, 0));
   };
 
+  console.log(answers);
   return (
-    <Box sx={{ height: "100vh", overflow: "hidden" }}>
+    <Box sx={{ height: "100vh", overflow: "hidden", position: "relative" }}>
       <TransitionGroup>
-        <CSSTransition key={currentQuestion} classNames="fade" timeout={300}>
+        <CSSTransition
+          key={currentQuestion}
+          timeout={300}
+          classNames="fade" // CSS sınıfı
+        >
           <Box
             sx={{
               width: "100%",
               height: "100vh",
               display: "flex",
+              flexDirection: "column",
               justifyContent: "center",
               alignItems: "center",
-              flexDirection: "column",
+              transition: "opacity 0.3s ease-in-out",
             }}
           >
-            <Typography
-              sx={{
-                mb: 2,
-                fontWeight: "xl",
-                fontSize: "xs",
-                letterSpacing: "1px",
-                width: "300px",
-              }}
-            >
-              {questions[currentQuestion].question}
-            </Typography>
-            {questions[currentQuestion].type === "multiple-choice" ? (
-              <RadioGroup
-                aria-labelledby="storage-label"
-                size="lg"
-                sx={{ gap: 1.5, width: "300px" }}
-                value={
-                  selectedValue || answers[questions[currentQuestion].id] || ""
-                }
-                onChange={handleRadioChange}
-              >
-                {questions[currentQuestion].options.map((value) => (
-                  <Sheet
-                    key={value}
-                    sx={{ p: 2, borderRadius: "md", boxShadow: "sm" }}
-                  >
-                    <Radio
-                      label={`${value}`}
-                      overlay
-                      disableIcon
-                      value={value}
-                    />
-                  </Sheet>
-                ))}
-              </RadioGroup>
-            ) : (
-              <TextField
-                variant="outlined"
-                placeholder={questions[currentQuestion].placeholder}
-                value={answers[questions[currentQuestion].id] || ""}
-                onChange={handleInputChange}
-                sx={{ width: "300px", mt: 2 }}
-              />
-            )}
-            <Box
-              sx={{ display: "flex", flexDirection: "column", width: "300px" }}
-            >
-              {required ? (
-                <Box  sx={{
-                  backgroundColor: "#F7E6E5",
-                  color: "#bc1616",
-                  p: "0.5rem",
-                  mt: "0.5rem",
-                  mb: "-1rem",
-                  borderRadius: "0.2rem",
-                  width: "9rem",
-                  textAlign: "center",
-                  display:"flex",
-                  gap:"0.2rem"
-                }}>
-                  <WarningRoundedIcon style={{fontSize:"0.98rem"}}/>
-                  <Typography
-                   sx={{fontSize: "0.8rem"}}
-                  >
-                    Please fill this in
+{/* Question 0 */}
+{currentQuestion === 0 && (
+              <>
+                <Typography
+                  sx={{
+                    mb: 2,
+                    fontWeight: "xl",
+                    textTransform: "uppercase",
+                    fontSize: "xs",
+                    letterSpacing: "0.15rem",
+                    textAlign: "left",
+                    width: "300px",
+                  }}
+                >
+                  How old are you?
+                </Typography>
+                <RadioGroup
+                  sx={{ width: "300px", gap: 1.5 }}
+                  value={answers[currentQuestion] || ""}
+                  onChange={handleRadioChange}
+                >
+                  {["Under 18", "18-24", "25-34", "35-44", "45-54", "55+"].map(
+                    (value) => (
+                      <Sheet key={value} sx={{ p: 2, borderRadius: "md", boxShadow: "sm" }}>
+                        <Radio label={value} overlay disableIcon value={value} />
+                      </Sheet>
+                    )
+                  )}
+                </RadioGroup>
+                {required && (
+                  <Typography sx={{ color: "red", mt: 2 }}>
+                    Please answer this question.
                   </Typography>
-                </Box>
-              ) : (
-                <Box sx={{ height: "1.7rem" }}></Box>
-              )}
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: 2,
-                  mt: 3,
-                  justifyContent: "center",
-                }}
-              >
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handlePrev}
-                  disabled={currentQuestion === 0}
+                )}
+              </>
+            )}
+{/* Question 1 */}
+{currentQuestion === 1 && (
+              <>
+                <Typography
+                  sx={{
+                    mb: 2,
+                    fontWeight: "xl",
+                    textTransform: "uppercase",
+                    fontSize: "xs",
+                    letterSpacing: "0.15rem",
+                    textAlign: "left",
+                    width: "300px",
+                  }}
                 >
-                  Back
-                </Button>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNext}
+                  What's your First Name?
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  sx={{ width: "300px", mt: 2 }}
+                  value={answers[currentQuestion] || ""}
+                  onChange={handleInputChange}
+                />
+                {required && (
+                  <Typography sx={{ color: "red", mt: 2 }}>
+                    Please answer this question.
+                  </Typography>
+                )}
+              </>
+            )}
+
+{/* Question 2 */}
+            {currentQuestion === 2 && (
+              <>
+                <Typography
+                  sx={{
+                    mb: 2,
+                    fontWeight: "xl",
+                    textTransform: "uppercase",
+                    fontSize: "xs",
+                    letterSpacing: "0.15rem",
+                    textAlign: "left",
+                    width: "300px",
+                  }}
                 >
-                  Next
-                </Button>
-              </Box>
-            </Box>
+                  What's your Last Name?
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  sx={{ width: "300px", mt: 2 }}
+                  value={answers[currentQuestion] || ""}
+                  onChange={handleInputChange}
+                />
+                {required && (
+                  <Typography sx={{ color: "red", mt: 2 }}>
+                    Please answer this question.
+                  </Typography>
+                )}
+              </>
+            )}
+
+
           </Box>
         </CSSTransition>
       </TransitionGroup>
+
+
+//! Arrow Icons
+      <Box
+        sx={{
+          position: "fixed",
+          zIndex: 3,
+          bottom: "2rem",
+          right: "2rem",
+          display: "flex",
+          gap: "0.5rem",
+          justifyContent: "center",
+        }}
+      >
+        <ArrowUpwardIcon
+          onClick={handlePrev}
+          sx={{
+            backgroundColor: "#0544AF",
+            color: "white",
+            borderRadius: "0.5rem",
+            fontSize: "2rem",
+            p: "0.2rem",
+            cursor: "pointer",
+            visibility: currentQuestion === 0 ? "hidden" : "visible", // İlk soruda yukarı oku gizle
+          }}
+        />
+        <ArrowDownwardIcon
+          onClick={handleNext}
+          sx={{
+            backgroundColor: "#0544AF",
+            color: "white",
+            borderRadius: "0.5rem",
+            fontSize: "2rem",
+            p: "0.2rem",
+            cursor: "pointer",
+          }}
+        />
+      </Box>
     </Box>
   );
 };
